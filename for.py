f5802e591e1612988f39119d947f1da012919a5a
AlexeyChernobuk <alexchernobuk3@gmail.com>
2024-11-23 15:40:07 +0400

    add file

--- for.py
+++ for.py
@@ -1,19 +1,25 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-
 primes = []
 not_primes = []
 
-for number in numbers:
-    if number == 1:
-        continue
-    is_prime = True
-    for i in range(2, number):
-        if number % i == 0:
-           is_prime = False
-    if is_prime:
-        primes.append(number)
-    else:
-        not_primes.append(number)
+for num in numbers:
+    count_p = 0
+    is_prime = bool
+    if num >= 2:
+        for j in range(1, num):
+            p = num % j
+            if p == 0 and j >= 1:
+                count_p = count_p + 1
+            else:
+                count_p = count_p
+        if count_p == 1:
+            is_prime = True
+        else:
+            is_prime = False
+        if is_prime:
+            primes.append(num)
+        else:
+            not_primes.append(num)
 
-print("Primes:", primes)
-print("Not Primes:", not_primes)
+print (f"Простые числа: {primes}")
+print (f"Непростые числа: {not_primes}")
